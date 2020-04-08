8bc86fdd50e9509e82ae41377ade664ab3854217
kjmj <kmorton15@gmail.com>
2020-04-07 22:24:24 -0400

    Refactored album card into its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialElevationOnHoverDirective } from './directives/material-elevation-on-hover.directive';
 import { ListFilterPipe } from './pipes/list-filter.pipe';
-import {MatInputModule} from '@angular/material/input';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AlbumRankComponent } from './components/album-rank/album-rank.component';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { AlbumTextComponent } from './components/album-text/album-text.component';
 import { ProjectDescriptionComponent } from './components/project-description/project-description.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ProjectHeaderComponent } from './components/project-header/project-header.component';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AlbumCardComponent } from './components/album-card/album-card.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +34,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     AlbumTextComponent,
     ProjectDescriptionComponent,
     ProjectHeaderComponent,
+    AlbumCardComponent,
   ],
   imports: [
     BrowserModule,
@@ -47,6 +50,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     ReactiveFormsModule,
     MatToolbarModule,
     MatProgressBarModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
